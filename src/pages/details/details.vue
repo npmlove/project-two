59a96798f8fc6686f927c1e96c333b88cc103ee8
QJH <15618972452@163.com>
2021-09-27 19:50:26 +0800

    删除vue项目

--- src/pages/details/details.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div style="color: #F00;padding: 50px;">我是详情页</div>
-</template>
-
-<script>
-</script>
-
-<style>
-</style>
--- src/service/index.js
+++
@@ -1,9 +0,0 @@
-var publicWeb = '/dcErp/faw'
-
-
-const serviceApi = {
-  // 首页Banner广告展示
-  showBanners: publicWeb+'/t-banner/showBanners',
-
-}
-export default serviceApi
