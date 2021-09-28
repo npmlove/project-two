017b3ba15b094dada54cc49aa20316c1505ff408
QJH <15618972452@163.com>
2021-09-28 19:46:55 +0800

    尾部

--- service/index.js
+++ service/index.js
@@ -1,6 +1,6 @@
 // var websocketUrl = 'wss://17dc.shenghuoq.com/back/dcPortal/user/' //开发环境
 // var websocketUrl = 'wss://17dc.shenghuoq.com/back/dcPortalTest/user/' //测试环境
-var websocketUrl = 'wss://www.17dc.com/back/dcPortal/user/' //生成环境
+// var websocketUrl = 'wss://www.17dc.com/back/dcPortal/user/' //生成环境
 
 
 
