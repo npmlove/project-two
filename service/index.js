ac2bbde84a7345cbab330b83a5bb1f5a8499a331
QJH <15618972452@163.com>
2021-10-12 17:09:07 +0800

    配置

--- service/index.js
+++ service/index.js
@@ -5,12 +5,12 @@ var websocketUrl = 'wss://17dc.shenghuoq.com/back/dcPortal/user/' //开发环境
 
 //开发环境
 // var webIp = 'https://17dc.shenghuoq.com'
-var webIp = 'http://10.8.0.1'
-var publicWeb = '/back/dcPortal/faw'
+// var webIp = 'http://10.8.0.1'
+// var publicWeb = '/back/dcPortal/faw'
 
 //测试环境或生成环境
-// var webIp = ''
-// var publicWeb = '/back/dcPortal/faw'
+var webIp = ''
+var publicWeb = '/back/dcPortal/faw'
 
 
 //接口
